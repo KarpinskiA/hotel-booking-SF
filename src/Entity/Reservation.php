bc02035362ce0023c142b55f14d6e5beb646374b
KarpinskiA <contact.karpinski@gmail.com>
2024-05-29 14:54:28 +0200

    Création des entitées Hotel, Reservation et Room

---
+++ src/Entity/Reservation.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $checkIn = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $checkOut = null;
+
+    #[ORM\Column]
+    private ?int $totalDays = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCheckIn(): ?\DateTimeInterface
+    {
+        return $this->checkIn;
+    }
+
+    public function setCheckIn(\DateTimeInterface $checkIn): static
+    {
+        $this->checkIn = $checkIn;
+
+        return $this;
+    }
+
+    public function getCheckOut(): ?\DateTimeInterface
+    {
+        return $this->checkOut;
+    }
+
+    public function setCheckOut(\DateTimeInterface $checkOut): static
+    {
+        $this->checkOut = $checkOut;
+
+        return $this;
+    }
+
+    public function getTotalDays(): ?int
+    {
+        return $this->totalDays;
+    }
+
+    public function setTotalDays(int $totalDays): static
+    {
+        $this->totalDays = $totalDays;
+
+        return $this;
+    }
+}
